719d94068148c3278194a004dab11f7339074efc
Kuriko <e155407w@st.u-gakugei.ac.jp>
2018-02-24 21:53:35 +0900

    Adapter 問題２

--- src/adapter/Teacher.java
+++ src/adapter/Teacher.java
@@ -2,7 +2,7 @@ package adapter;
 
 public class Teacher {
 	public static void main(String args[]){
-        Chairperson chairperson = new NewTaro();
+        Chairperson chairperson = new Hanako();
         chairperson.organizeClass();
     }
 }
