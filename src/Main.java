47778ee22d3f83be529330323bc7a7139ab9e7fa
Kuriko <e155407w@st.u-gakugei.ac.jp>
2018-02-24 16:41:43 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+	public static void main(String args[]){
+		Teacher you = new MyTeacher();
+        you.createStudentList();
+        you.callStudents();
+	}
+
+}
---
+++ src/MyStudentListIterator.java
@@ -0,0 +1,26 @@
+public class MyStudentListIterator implements Iterator{
+	private MyStudentList myStudentList;
+	private int index;
+
+	public MyStudentListIterator(MyStudentList list){
+		this.myStudentList = list;
+		this.index = 0;
+	}
+
+	public boolean hasNext() {
+		if(myStudentList.getLastNum() > index){
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+	public Student next() {
+		Student student = myStudentList.getStudentAt(index);
+		index++;
+		return student;
+	}
+
+
+}
