14fb4762456f59ffc9834f913364ef467360886f
Kuriko <e155407w@st.u-gakugei.ac.jp>
2018-02-25 18:42:42 +0900

    Chain of esponsibility 終了

---
+++ src/chainOfResponsibility/Question.java
@@ -0,0 +1,11 @@
+package chainOfResponsibility;
+
+import java.util.logging.Level;
+
+public class Question {
+
+	public Question(String string, Level level) {
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+}
