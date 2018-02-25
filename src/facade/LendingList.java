9b8ddaa89e7f30e8d6349331edd82303f6216a03
Kuriko <e155407w@st.u-gakugei.ac.jp>
2018-02-25 19:12:41 +0900

    facade 終了

---
+++ src/facade/LendingList.java
@@ -0,0 +1,10 @@
+package facade;
+
+//所蔵本リスト
+public class LendingList {
+	public boolean check(String bookName) {
+        //貸出帳をチェックする
+        //貸出中ならtrue、そうでなければfalseを返す
+        return true;
+    }
+}
