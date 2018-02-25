9383efaca2933441a67d039be2ee7a700beab157
Kuriko <e155407w@st.u-gakugei.ac.jp>
2018-02-25 19:28:52 +0900

    mediator 終了

---
+++ src/mediator/Colleague.java
@@ -0,0 +1,6 @@
+package mediator;
+
+public interface Colleague {
+	// 恋愛相談をする
+
+}
---
+++ src/mediator/Nitta.java
@@ -0,0 +1,18 @@
+package mediator;
+
+public class Nitta implements Colleague{
+    private String name = "新田";
+    private int tension;
+    private Colleague secretLover = null;
+    private LoveMediator mediator = new Saito();
+
+    public String getName(){
+        return name;
+    }
+    public  void setSecretLover(Colleague colleague){
+        this.secretLover = colleague;
+    }
+    public void needsAdvice(){
+        this.tension = mediator.consultation(this,secretLover);
+    }
+}
